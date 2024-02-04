6b3ed2aed0fde6023a668d63c1e22251ef07a256
Gunwoo Kim <gunwoolouie@gmail.com>
2024-02-04 00:31:25 -0800

    Keyboard demo documentation.

--- sw/src/keyboard_demo.py
+++ sw/src/keyboard_demo.py
@@ -1,3 +1,9 @@
+"""
+This program is intended to be used as an interface between the classifier and the game.
+The classifier will have arbitrary outputs, i.e. 0, 1. We now map these keystrokes into
+correct keystrokes for the game Tetris. 
+"""
+
 import keyboard
 
 keyboard.remap_hotkey("0", "left")
