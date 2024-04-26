d6cc7e4204b628e0d913a0656b49399faaeb0ebb
Gunwoo Kim <gunwoolouie@gmail.com>
2024-04-26 01:18:27 -0700

    Only sends input when Tetris game window is active.

--- sw/src/classifier_tetris_interface.py
+++ sw/src/classifier_tetris_interface.py
@@ -6,19 +6,36 @@ correct keystrokes for the game Tetris.
 
 import keyboard
 import pylsl
+import pygetwindow as gw
+import time
+
+GAME_WINDOW_NAME = 'Tetris'
 
 result_inlet = None
 info = pylsl.resolve_stream('name', "Classifier_Result_Out")
 result_inlet = pylsl.stream_inlet(info[0], recover=False)
 print(f'mapper has received the {info[0].type()} inlet.')
 
-keyboard.remap_hotkey("0", "left")
-keyboard.remap_hotkey("1", "right")
-
 while True:
     res, time = result_inlet.pull_sample(timeout=0)
+
+    found = False
+    wins = gw.getWindowsWithTitle(GAME_WINDOW_NAME)
+    for win in wins:
+        if win.title == GAME_WINDOW_NAME and win.isActive:
+            found = True
+            break
+
+    if not found:
+        print("Tetris window not open. Waiting for it to be open again.")
+        continue
 
     if res is not None:
         mvt = res[0]
         print(f'i got {mvt}')
-        keyboard.press_and_release(mvt)
+        if (mvt == 'left'):
+            keyboard.press_and_release('a') # rotate CCW
+        elif (mvt == 'right'):
+            keyboard.press_and_release('right') # move right
+        
+        # otherwise do nothing.
